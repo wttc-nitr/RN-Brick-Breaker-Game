02be1192ad6b461cff7042c22810b7b80bd7b1c9
Animesh Kumar <kumaranimesh097@gmail.com>
2025-06-03 16:11:32 +0530

    add React-Native-Gesture-Handler

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import Game from "@/components/Game";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Game />
-      <StatusBar style="light" />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Game />
+        <StatusBar style="light" />
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
