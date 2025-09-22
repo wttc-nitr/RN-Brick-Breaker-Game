bd97097d38449c0d03c21c00eb472efe9791e640
Animesh Kumar <kumaranimesh097@gmail.com>
2025-09-22 19:00:40 +0530

    fix status bar in dark theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import Game from "@/components/Game";
 import GameProvider from "@/providers/GameProvider";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const colorScheme = useColorScheme() ?? "light";
   return (
     <SafeAreaProvider>
+      <StatusBar
+        backgroundColor={colorScheme === "dark" ? "black" : "white"}
+        style={colorScheme === "dark" ? "light" : "dark"}
+      />
       <SafeAreaView style={{ flex: 1 }}>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <GameProvider>
